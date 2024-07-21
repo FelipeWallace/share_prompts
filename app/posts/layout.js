b99ff5a7048560b573b2e85fd02823b495a72723
Felipe Wallace <felipewallacepc@gmail.com>
2024-07-21 12:35:10 -0300

    Introdução

---
+++ app/posts/layout.js
@@ -0,0 +1,11 @@
+/* Componentes aqui irao afetar somente os posts*/
+
+import React from "react";
+
+const layout = () => {
+  return (
+    <div>Navigate to top</div>
+  )
+}
+
+export default layout
